a68fce066db47fde13c92625254bc0ee828890e1
DarKreter <darkreter@gmail.com>
2022-09-19 19:58:04 +0200

    Testing sending messages

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from unidecode import unidecode
 from TOKEN import TOKEN
 
 intents = discord.Intents.all()
@@ -6,6 +7,22 @@ client = discord.Client(intents=intents)
 
 # from functions import *
 # from globalVar import *
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    mess = unidecode(message.content)
+    
+    await message.channel.send(mess)
 
 
 client.run(TOKEN)
