c023a9f4eb36fa843b0e5a4f49ece3c53c324213
DarKreter <darkreter@gmail.com>
2022-09-19 20:34:37 +0200

    Add dictionary with everyone ID

--- globalVar.py
+++ globalVar.py
@@ -1,32 +1,8 @@
-# wybieraczRoliMessageID = 758457353959505941 #Do zmiany
-# wybieraczRoliChannelID = 758436604955328532 #Do zmiany
-# wybieraczRoliEmojiName = "🕵️‍♂️"               #Do zmiany
-
-# instrukcjaMessageID = 804056629506080768 
-# instrukcjaChannelID = 803992418922790942 
-# instrukcjaEmojiName = "W4" 
-
-# pytanie1MessageID = 804056355824468000
-# pytanie1ChannelID = 803992458929242144 
-
-# pytanie2MessageID = 804056393136865351 
-# pytanie2ChannelID = 803992482253504542 
-
-# pytanie3MessageID = 804056467539361832 
-# pytanie3ChannelID = 803992504424857627 
-
-# pytaniaEmoji1    = "1_" 
-# pytaniaEmoji2    = "2_" 
-# pytaniaEmoji3    = "3_" 
-# pytaniaEmoji4    = "4_"  
- 
-# gratulacjeMessageID = 804056526611677234 
-# gratulacjeChannelID = 803992529401413662 
-# gratulacjeEmojiName = "🍺"
-
-
-# loseChannelID           = 804052256873906267 
-# loseMessageContent      = "Kocham piwo!" 
-
-
-# wybieraczRoliRoleToAddName = "DZIEKAN IMPOSTOR?"
+users = {
+    "Michal":   419239403584749569,
+    "Szewczyk": 280099473064198154,
+    "Witek":    443117367598383104,
+    "Daniel":   324627270478200832,
+    "Jedrzej":  258945670734544897,
+    "Bartpi":   244869449641885699
+}
