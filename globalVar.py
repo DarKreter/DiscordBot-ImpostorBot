2902036b96216ff92129006e432e8f773116295b
DarKreter <darkreter@gmail.com>
2022-09-19 19:42:00 +0200

    Init

---
+++ globalVar.py
@@ -0,0 +1,32 @@
+# wybieraczRoliMessageID = 758457353959505941 #Do zmiany
+# wybieraczRoliChannelID = 758436604955328532 #Do zmiany
+# wybieraczRoliEmojiName = "🕵️‍♂️"               #Do zmiany
+
+# instrukcjaMessageID = 804056629506080768 
+# instrukcjaChannelID = 803992418922790942 
+# instrukcjaEmojiName = "W4" 
+
+# pytanie1MessageID = 804056355824468000
+# pytanie1ChannelID = 803992458929242144 
+
+# pytanie2MessageID = 804056393136865351 
+# pytanie2ChannelID = 803992482253504542 
+
+# pytanie3MessageID = 804056467539361832 
+# pytanie3ChannelID = 803992504424857627 
+
+# pytaniaEmoji1    = "1_" 
+# pytaniaEmoji2    = "2_" 
+# pytaniaEmoji3    = "3_" 
+# pytaniaEmoji4    = "4_"  
+ 
+# gratulacjeMessageID = 804056526611677234 
+# gratulacjeChannelID = 803992529401413662 
+# gratulacjeEmojiName = "🍺"
+
+
+# loseChannelID           = 804052256873906267 
+# loseMessageContent      = "Kocham piwo!" 
+
+
+# wybieraczRoliRoleToAddName = "DZIEKAN IMPOSTOR?"
